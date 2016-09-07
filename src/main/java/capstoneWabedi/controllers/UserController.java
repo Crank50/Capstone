e17343f916d5ad965166cc6c751e86d66025a821
Justin <Crank50@icloud.com>
2016-09-07 09:42:13 -0600

    8 commit

---
+++ src/main/java/capstoneWabedi/controllers/UserController.java
@@ -0,0 +1,32 @@
+package capstoneWabedi.controllers;
+
+import capstoneWabedi.entities.User;
+import capstoneWabedi.entities.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ * Created by Justin on 9/1/16.
+ */
+@Controller
+@RequestMapping(value="/user/")
+public class UserController {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserController(UserDao userDao) {
+        Assert.notNull(userDao, "UserDao must not be null!");
+        this.userDao = userDao;
+    }
+
+    @RequestMapping(value="saveNewUser")
+    public View saveNewQuestion(User user) {
+        userDao.save(user);
+        return new RedirectView("/user/");
+    }
+}
--- src/main/resources/META-INF/resources/WEB-INF/jsp/Users/createAccount.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/Users/createAccount.jsp
@@ -1,11 +1,15 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
 <head>
     <title>WWAD Create Account </title>
 
 </head>
 <body>
+
+<form:label path="name">Name: </form:label>
+<form:input path="name" id="name" maxlength="30" size="100" />
 -----form----
 name
 state, city, zip
