553d8462f9f56d206f7403e0196bf924237daf36
Justin <Crank50@icloud.com>
2016-09-12 15:54:17 -0600

    initial commit

--- src/main/java/capstoneWabedi/controllers/UserController.java
+++ src/main/java/capstoneWabedi/controllers/UserController.java
@@ -29,8 +29,8 @@ public class UserController {
         return "/Users/createAccount";}
 
     @RequestMapping(value ="/userCreation")
-    public String svAcnnt(ModelMap modelMap) {
-        modelMap.addAttribute("user", new User());
+    public String svAcnnt(ModelMap modelMap,User user) {
+        modelMap.addAttribute("user", user);
         return "/Users/userCreation";}
 
 
