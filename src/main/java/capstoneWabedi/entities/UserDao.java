78983632c19b56bb90592b0907e86bfbb2833200
Justin <Crank50@icloud.com>
2016-09-13 14:56:52 -0600

    initial commit

--- src/main/java/capstoneWabedi/entities/UserDao.java
+++ src/main/java/capstoneWabedi/entities/UserDao.java
@@ -1,11 +1,13 @@
 package capstoneWabedi.entities;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by Justin on 9/1/16.
  */
+@Repository
 @Transactional
 public interface UserDao extends CrudRepository<User, Long> {
 
