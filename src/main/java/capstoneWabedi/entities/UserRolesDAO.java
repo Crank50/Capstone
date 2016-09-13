01a1cf28ae94791a6b2e7f2c1f7ae5c2652d8989
Justin <Crank50@icloud.com>
2016-09-13 14:48:26 -0600

    initial commit

--- src/main/java/capstoneWabedi/entities/UserRolesDAO.java
+++ src/main/java/capstoneWabedi/entities/UserRolesDAO.java
@@ -11,6 +11,6 @@ import java.util.List;
 
 public interface UserRolesDAO extends CrudRepository<UserRole, Long>{
 
-    @Query("select a.role from UserRole a, User b where b.userName=?1 and a.userid=b.userId")
-    public List<String> findRoleByUserName(String username);
+//    @Query("select a.role from UserRole a, User b where b.userName=?1 and a.userid=b.userId")
+//    public List<String> findRoleByUserName(String username);
 }
