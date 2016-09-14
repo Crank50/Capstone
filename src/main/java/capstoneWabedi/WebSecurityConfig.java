fd8ef4310ec4d7f2f6e4b84f154c4ca382df012f
Justin <Crank50@icloud.com>
2016-09-14 09:48:23 -0600

    initial commit

--- src/main/java/capstoneWabedi/WebSecurityConfig.java
+++ src/main/java/capstoneWabedi/WebSecurityConfig.java
@@ -27,7 +27,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/webUi/userPage", "/mainPage","/user/createAccount","/user/userCreation","/user/saveNewUser","/Users/userCreation","/Users/adminPage").permitAll()
+                .antMatchers("/", "/webUi/userPage", "/mainPage","/user/createAccount","/user/userCreation","/user/saveNewUser","/Users/userCreation","/admin/","/css/*").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
