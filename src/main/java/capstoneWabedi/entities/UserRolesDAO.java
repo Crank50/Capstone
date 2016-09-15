a5daffd339e0cc492ee0d4cd7038ab1457802715
Justin <Crank50@icloud.com>
2016-09-15 10:32:04 -0600

    initial commit

--- src/main/java/capstoneWabedi/entities/UserRolesDAO.java
+++ src/main/java/capstoneWabedi/entities/UserRolesDAO.java
@@ -11,7 +11,7 @@ import java.util.List;
  * Created by Justin on 9/13/16.
  */
 @Repository
-
+@Transactional (noRollbackFor = Exception.class)
 public interface UserRolesDAO extends CrudRepository<UserRole, Long>{
 
     @Query("select a.role from UserRole a, User b where b.userName=?1 and a.userId=b.userId")
