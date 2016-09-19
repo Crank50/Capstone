fe92206dab3032259a2a0373110f0b7a2f4752dc
Justin <Crank50@icloud.com>
2016-09-19 10:49:15 -0600

    initial commit

--- src/main/java/capstoneWabedi/controllers/UserController.java
+++ src/main/java/capstoneWabedi/controllers/UserController.java
@@ -64,7 +64,7 @@ public class UserController {
         userDao.save(user);
 
 
-        return "/WebUi/userPage";
+        return "/Users/userAccountPage";
     }
     @RequestMapping(value="/adminPage")
     public String adminPage() {
